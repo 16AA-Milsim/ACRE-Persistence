7fc00930e250d5b43738616dc4ba1c73d6047051
MildlyInterested <mueller.manuel@mailbox.org>
2023-09-29 21:41:09 +0200

    Final media

--- meta.cpp
+++ meta.cpp
@@ -2,10 +2,10 @@
  * File: meta.cpp
  * Author: Mildly_Interested
  * Date: 2023-09-23
- * Last Update: 2023-09-24
+ * Last Update: 2023-09-29
  * License: GNU General Public License v3.0 only - https://www.gnu.org/licenses/gpl-3.0-standalone.html
  */
 
 protocol = 1;
-publishedid = 123456790; //TODO update!
+publishedid = 3043180500;
 name = "ACRE-Persistence";
